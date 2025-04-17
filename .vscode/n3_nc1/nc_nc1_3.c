98b516e804a1d49656a5e57d9ce0c892176a8e2d
Wilmarbo <wilmarbo@hotmail.com>
2025-04-17 01:30:11 +0000

    atulizacao de aulas

--- .vscode/n3_nc1/nc_nc1_3.c
+++ .vscode/n3_nc1/nc_nc1_3.c
@@ -5,7 +5,7 @@ int main(){
     double numeroMuitoPreciso = 3.141592653589793238463;
 
     printf("Numero preciso (double): %.15f\n", numeroPreciso);
-    printf("Numero muito preciso (long double): %.21lf\n", numeroMuitoPreciso);
+    printf("Numero muito preciso (long double): %. 21lf\n", numeroMuitoPreciso);
 
     return 0;
 }
